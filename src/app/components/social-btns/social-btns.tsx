d0d210e283d67d604da8a3790b539b0d2f3acf42
CaianMarcinkowski <caianferre@gmail.com>
2023-09-21 15:08:14 -0300

    feat: animation social buttons

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -1,17 +1,47 @@
+"use client";
 import { DiscordIcon } from "../icons/discord-icon";
 import { GitIcon } from "../icons/git-icon";
 import { InstaIcon } from "../icons/insta-icon";
 import { LinkedinIcon } from "../icons/linkedin-icon";
+import { motion } from "framer-motion";
 
-import "./social-btns.scss"
+import "./social-btns.scss";
 
-export function SocialBtns(){
-    return(
-        <div className="social">
-            <a href="https://www.instagram.com/marcinkowski__/?next=%2F"><InstaIcon/></a>
-            <a href="https://github.com/CaianMarcinkowski"><GitIcon/></a>
-            <a href="https://www.linkedin.com/in/caian-marcinkowski-ferreira-bb541a137/"><LinkedinIcon/></a>
-            <a href="https://discord.gg/KrhJ4mF6MK"><DiscordIcon/></a>
-        </div>
-    )
+export function SocialBtns() {
+  return (
+    <div className="social">
+      <motion.a
+        href="https://www.instagram.com/marcinkowski__/?next=%2F"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      >
+        <InstaIcon />
+      </motion.a>
+      <motion.a
+        href="https://github.com/CaianMarcinkowski"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      >
+        <GitIcon />
+      </motion.a>
+      <motion.a
+        href="https://www.linkedin.com/in/caian-marcinkowski-ferreira-bb541a137/"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      >
+        <LinkedinIcon />
+      </motion.a>
+      <motion.a
+        href="https://discord.gg/KrhJ4mF6MK"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+      >
+        <DiscordIcon />
+      </motion.a>
+    </div>
+  );
 }
